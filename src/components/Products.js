ed4f4ccb002f8718c42d9bc7436ee6127432445a
maksigit <maksi123@ukr.net>
2019-06-12 12:43:24 +0300

    add status 0 item on sever

--- src/components/Products.js
+++ src/components/Products.js
@@ -23,6 +23,20 @@ class Products extends Component {
             .then(products => this.props.toLoad(products))
     };
 
+    // removeItem = (id) => {
+    //     fetch('https://gentle-escarpment-19443.herokuapp.com/v1/articles/' + id, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             'Authorization': this.getTokenFromLS()
+    //         }
+    //     }).then(res => res.json())
+    //         .then(products => this.props.toLoad(products))
+    // };
+
+
+
     render() {
         var removeById = (id) => {
             this.props.removeItem(id)
@@ -32,22 +46,25 @@ class Products extends Component {
                 <button onClick={this.getProducts}>To load Products</button>
                 <div className='wrap-all-products'>
                     {this.props.testStore != null ? this.props.testStore.map(function (item, i) {
-                        return <div key={item.id} className='item'>
-                            <div className='item-name'>
-                                <span className='text'>Name: </span>
-                                <span>{item.name}</span>
+                        if (item.status !== 0) {
+                            return <div key={item.id} className='item'>
+                                <div className='item-name'>
+                                    <span className='text'>Name: </span>
+                                    <span>{item.name}</span>
+                                </div>
+                                <div className='item-desc'>
+                                    <span className='text'>Desc: </span>
+                                    <span>{item.description}</span>
+                                </div>
+                                <div className='item-price'>
+                                    <span className='text'>Price: </span>
+                                    <span>{item.price}</span>
+                                </div>
+                                <div>id - {item.id}</div>
+                                <div>status - {item.status}</div>
+                                <div className='del-item' onClick={() => removeById(item.id)}>x</div>
                             </div>
-                            <div className='item-desc'>
-                                <span className='text'>Desc: </span>
-                                <span>{item.description}</span>
-                            </div>
-                            <div className='item-price'>
-                                <span className='text'>Price: </span>
-                                <span>{item.price}</span>
-                            </div>
-                            <div>{item.id}</div>
-                            <div className='del-item' onClick={() => removeById(item.id)}>x</div>
-                        </div>
+                        }
                     }) : ' '}
                 </div>
             </div>
--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -1,5 +1,26 @@
+function getTokenFromLS() {
+    return localStorage.getItem('token')
+}
+
 function removeItem(temp, id) {
-    return temp.filter(item => id !== item.id);
+    return temp.map(function (item) {
+        if (item.id === id) {
+            item.status = 0;
+            fetch('https://gentle-escarpment-19443.herokuapp.com/v1/articles/' + id, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': getTokenFromLS()
+                },
+                body: JSON.stringify({status: 0})
+            }).then(res => res.json())
+                .then(products => console.log('DEL', products))
+        }
+        return item
+    })
+
+    // return temp.filter(item => id !== item.id);
 }
 
 function toLoad(temp, obj=[]) {
